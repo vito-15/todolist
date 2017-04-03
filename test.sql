2475bb39c4bd2626bbffb9593c94f617908fc134
V. Gadjimagamedov <gadjimagamedov@yandex.ru>
2017-04-03 13:12:16 +0300

    test

--- test.sql
+++ test.sql
@@ -23,7 +23,7 @@ DROP TABLE IF EXISTS `task`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `task` (
-  `id` mediumint(9) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `text` varchar(1000) NOT NULL,
   `createdt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `expiredt` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
@@ -51,4 +51,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-04-03 11:48:31
+-- Dump completed on 2017-04-03 11:57:29
